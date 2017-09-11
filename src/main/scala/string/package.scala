b603b55f8e2decfb5b1459813126b1d2b02b92c3
Ignat Loskutov <ignat.loskutov@gmail.com>
2017-09-11 16:04:56 +0300

    Add a few more combinators

--- src/main/scala/string/package.scala
+++ src/main/scala/string/package.scala
@@ -2,14 +2,30 @@ import core._
 import cats.implicits._
 
 package object string {
-  def satisfy(p: Char => Boolean): Parser[String, Char] = Parser { input =>
+  type StringParser[T] = Parser[String, T]
+
+  def satisfy(p: Char => Boolean): StringParser[Char] = Parser { input =>
     input.headOption match {
       case Some(c) if p(c) => Some(input.tail, c)
       case _               => None
     }
   }
 
-  def char(c: Char): Parser[String, Char] = satisfy(c == _)
+  def char(c: Char): StringParser[Char] = satisfy(c == _)
 
-  def str(s: String): Parser[String, String] = s.toList.map(char).sequence.map(_.mkString)
+  def charNot(c: Char): StringParser[Char] = satisfy(c != _)
+
+  def str(s: String): StringParser[String] = s.toList.map(char).sequence.map(_.mkString)
+
+  def anyOf(s: String): StringParser[Char] = satisfy(s.contains(_))
+
+  def noneOf(s: String): StringParser[Char] = satisfy(!s.contains(_))
+
+  implicit class StringParserOps[T](p: StringParser[T]) {
+    def sepBy(sep: StringParser[_]): StringParser[Seq[T]] =
+      for {
+        head <- p
+        rest <- many(sep *> p)
+      } yield head +: rest
+  }
 }
