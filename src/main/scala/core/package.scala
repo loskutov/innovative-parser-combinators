a590ade98b6083c12cc8bed941caeb1ac0b74748
Ignat Loskutov <ignat.loskutov@gmail.com>
2017-09-11 12:57:36 +0300

    Use `<+>` instead of self-written `<|>`

--- src/main/scala/core/package.scala
+++ src/main/scala/core/package.scala
@@ -10,13 +10,7 @@ package object core {
     def apply[T, V](f: T => Option[(T, V)]): Parser[T, V] = StateT.apply(f)
   }
 
-  implicit class ParserOps[T, V](p: Parser[T, V]) {
-    def <|>(q: Parser[T, V]): Parser[T, V] = Parser { input =>
-      p.run(input) orElse q.run(input)
-    }
-  }
-
-  def many[T, V](parser: Parser[T, V]): Parser[T, Seq[V]] = some(parser) <|> pure(Seq.empty)
+  def many[T, V](parser: Parser[T, V]): Parser[T, Seq[V]] = some(parser) <+> pure(Seq.empty)
 
   def some[T, V](parser: Parser[T, V]): Parser[T, Seq[V]] =
     for {
